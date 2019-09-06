c4909d00f52b9492fd0dacb339c1aa92d74e7b3c
Evan Sarmiento <esarmien@g.harvard.edu>
2019-09-06 13:05:53 -0400

    Revert

--- run.R
+++ run.R
@@ -8,12 +8,6 @@ if (length(args) == 0) {
 }
 
 print(paste("Listening on port", port))
-dir.create("/app/tmp")
 rmarkdown::run(dir=getwd(),
-	       render_args = list(output_dir="/app/tmp",
-			          intermediates_dir="/app/tmp/intermediates",
-				  clean=FALSE,
-				  quiet=TRUE
-			     ),
                shiny_args = list(host = "127.0.0.1",
                                  port = as.numeric(port)))
